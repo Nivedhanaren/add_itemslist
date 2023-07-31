8b022ef91699e8013fa077f4fa7af113118bd8d8
Nivedha Virushabadoss <nivedhavirushabadoss@gmail.com>
2023-07-31 09:41:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import './style.css'
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [nitems, setNitems] = useState("");
+  function onRemoveItem(itemToRemove) {
+    const newItems = items.filter((item) => {
+      return item !== itemToRemove;
+    });
+    setItems(newItems);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    if (!nitems) return;
+    const listItems = [...items, nitems];
+    setItems(listItems);
+    setNitems("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Add Shopping List</h1>
+      <div className="shopping-list">
+        <h2>List to buy</h2>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="item"
+            placeholder="Add a new item"
+            required
+            value={nitems}
+            onChange={(e) => setNitems(e.target.value)}
+          ></input>
+          <button>ADD</button>
+        </form>
+        <ul>
+          {items.map((item, index) => (
+            <Item onRemoveItem={onRemoveItem} item={item} key={index}></Item>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
-
+function Item({ onRemoveItem, item }) {
+  return (
+    <li>
+      {item}
+      <button className="delete" onClick={() => onRemoveItem(item)}>x</button>
+    </li>
+  );
+}
 export default App;
